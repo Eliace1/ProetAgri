28b70fc2d359485e552f1466fe20a9d770141236
Eliace <tacalada@3il.fr>
2025-10-01 13:17:55 +0200

    bd complet

---
+++ backend/app/Models/Commande.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Commande extends Model
+{
+    protected $fillable=[
+        'total_amount',
+        'delivery_address',
+        'payment_method',
+    ];
+
+    /**
+     * retourne l'utilisateur d'une commande
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     *
+     * Retourne la liste des produits d'une commande
+     */
+    public function products() {
+        return $this->belongsToMany(Product::class, 'commande_produit')
+                    ->withPivot('quantite')
+                    ->withTimestamps();
+    }
+}
